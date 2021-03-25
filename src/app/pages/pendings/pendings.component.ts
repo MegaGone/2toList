85691791373a03af2ebfb8887ea341a1a32afe4c
Jimmy Martinez <68252026+MegaGone@users.noreply.github.com>
2021-03-24 19:45:05 -0600

    modal p1

--- src/app/pages/pendings/pendings.component.ts
+++ src/app/pages/pendings/pendings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap'
 @Component({
   selector: 'app-pendings',
   templateUrl: './pendings.component.html'
@@ -7,13 +8,33 @@ import { Router } from '@angular/router';
 
 export class PendingsComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  public closeResult: string | any;
+
+  constructor(private router: Router, private modalSvc: NgbModal) { }
 
   ngOnInit(): void {
   }
 
   createList(){
     this.router.navigate(['/list', 'new'])
+  }
+
+  open(content: string | any){
+    this.modalSvc.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`
+    })
+  }
+
+  private getDismissReason(reason: any): string {
+      if(reason === ModalDismissReasons.ESC){
+        return 'by pressing esc';
+      }else if(reason === ModalDismissReasons.BACKDROP_CLICK){
+        return 'by clicking backdrop'
+      }else{
+        return `with ${reason}`;
+      }
   }
 
 }
